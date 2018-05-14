c5ede94f499e4ea0148c236f11d65cfcca3a3e09
Aleksandar Gojkovic <gojkovic-frontend@hotmail.com>
2018-05-14 08:46:35 +0200

    chore: finalized homepage and about page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-$('.hamburgerBtn').on('click', function() {
+function menuToggle() {
+    $(this).toggleClass('hamburgerBtn--xMode');
     $('.header__nav').slideToggle(300);
+}
+
+function showModal(e) {
+    const modal = $(this).data('modal');
+
+    document.querySelector('body').classList.add('u-modal-visible');
+    document.querySelector('div[data-modal="' + modal +'"]').classList.add('modal__container--visible');
+
+    e.preventDefault();
+}
+
+function hideModal(e) {
+    if (!$(e.target).hasClass('modal')) {
+        document.querySelector('.modal__container--visible').classList.remove('modal__container--visible');
+        document.querySelector('body').classList.remove('u-modal-visible');
+    }
+}
+
+$('.hamburgerBtn').on('click', menuToggle);
+$('.js-modal-trigger').on('click', showModal);
+$('.modal__container').on('click', hideModal);
+$('.modal').on('click', function(e) {
+    e.stopPropagation();
 });
---
+++ js/plugins.js
@@ -0,0 +1,9 @@
+$('.photoSlider').bxSlider({
+    wrapperClass: 'photoSlider__container',
+    minSlides: 1,
+    maxSlides: 10,
+    slideWidth: 300,
+    ticker: true,
+    speed: 150000,
+    tickerHover: true
+});
